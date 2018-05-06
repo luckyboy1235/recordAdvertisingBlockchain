0d77e38884434a510e49b4c30d8358b55221344c
Đinh Hồng Phi <dinhhongphi@gmail.com>
2018-05-06 19:59:30 +0700

    play video and advetisement

--- client/src/app/components/video/video.component.ts
+++ client/src/app/components/video/video.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import * as videojs from 'video.js'
-
+import * as videojs from 'video.js';
+import '../../../../node_modules/videojs-contrib-ads/dist/videojs.ads.js'
+var adRunNext = 0
+var adInited = 0;
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
@@ -11,72 +13,89 @@ export class VideoComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    videojs('example_video_1', {
+    let player = videojs('example_video_1', {
       controls: true,
       autoplay: true,
       preload: true
     }, function () {
+    })
+    // Register the component with Video.js, so it can be used in players.
+    videojs.registerComponent('skipButton', SkipButton);
+    player.on('readyforpreroll', function () {
+      console.log('readyforpreroll');
+      player.ads.startLinearAdMode();
+      player.src('/video/1137981449796386429.mp4');
+      //trigger to remove spinner
+      player.on('adplaying', function () {
+        player.trigger('ads-ad-started');
+      });
 
+      // resume content when all your linear ads have finished
+      player.on('adended', function () {
+        player.ads.endLinearAdMode();
+      });
     })
 
-    // Get the Component base class from Video.js
-    var Component = videojs.getComponent('Component');
 
-    // The videojs.extend function is used to assist with inheritance. In
-    // an ES6 environment, `class TitleBar extends Component` would work
-    // identically.
-    var TitleBar = videojs.extend(Component, {
 
-      // The constructor of a component receives two arguments: the
-      // player it will be associated with and an object of options.
-      constructor: function (player, options) {
-
-        // It is important to invoke the superclass before anything else, 
-        // to get all the features of components out of the box!
-        Component.apply(this, arguments);
-
-        // If a `text` option was passed in, update the text content of 
-        // the component.
-        if (options.text) {
-          this.updateTextContent(options.text);
+    //show advetising every 15 minius
+    player.on('timeupdate', function () {
+      function shouldPlayAd(currentTime) {
+        let time = currentTime - adRunNext;
+        if (time >= 0) {
+          return true;
+        } else {
+          return false;
         }
-      },
+      }
 
-      // The `createEl` function of a component creates its DOM element.
-      createEl: function () {
-        return videojs.createEl('div', {
-
-          // Prefixing classes of elements within a player with "vjs-" 
-          // is a convention used in Video.js.
-          className: 'vjs-title-bar'
-        });
-      },
-
-      // This function could be called at any time to update the text 
-      // contents of the component.
-      updateTextContent: function (text) {
-
-        // If no text was provided, default to "Title Unknown"
-        if (typeof text !== 'string') {
-          text = 'Title Unknown';
+      var currentTime = player.currentTime();
+      console.log(currentTime);
+      if (shouldPlayAd(currentTime) && player.currentSrc() === "/video/7092191947868798372.mp4") {
+        adRunNext = currentTime + 119;
+        console.log('play ads');
+        if (!adInited) {
+          this.ads();
+          adInited = 1;
         }
-
-        // Use Video.js utility DOM methods to manipulate the content
-        // of the component's element.
-        videojs.emptyEl(this.el());
-        videojs.appendContent(this.el(), text);
+        
+        player.trigger('adsready');
+        player.addChild('skipButton', { text: 'Skip' });
       }
     });
-
-    // Register the component with Video.js, so it can be used in players.
-    videojs.registerComponent('TitleBar', TitleBar);
-
-    // Create a player.
-    var player = videojs('example_video_1');
-
-    // Add the TitleBar as a child of the player and provide it some text 
-    // in its options.
-    player.addChild('TitleBar', { text: 'Skip' });
   }
-
 }
+
+
+var ComponentVideoJS = videojs.getComponent('Component');
+
+let SkipButton = videojs.extend(ComponentVideoJS, {
+  constructor: function (player, options) {
+
+    ComponentVideoJS.apply(this, arguments);
+    //hidden skip button and ended advetisement
+    this.on('click', function () {
+      player.ads.endLinearAdMode()
+      player.trigger('ads-ad-started');
+      player.removeChild('skipButton');
+    });
+    if (options.text) {
+      this.updateTextContent(options.text);
+    }
+  },
+
+  createEl: function () {
+    return videojs.createEl('div', {
+      className: 'vjs-skip-button'
+    });
+  },
+
+  updateTextContent: function (text) {
+    if (typeof text !== 'string') {
+      text = 'Title Unknown';
+    }
+
+    videojs.emptyEl(this.el());
+    videojs.appendContent(this.el(), text);
+  }
+});
